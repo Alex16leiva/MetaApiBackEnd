32f7d233c7ff6128efa04a81980a1f5e5664c1ac
alex16leiva <alex16leiva@gmail.com>
2025-02-23 16:38:22 -0600

    Se agrego clase chathub, para enviar mensajes al front-end

---
+++ WebServices/Controllers/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebServices.Controllers
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string? fileUrl = null, string? fileType = null)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message, fileUrl, fileType);
+        }
+    }
+}
